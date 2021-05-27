495727a68c107a25425dbf85612235f95ab5f191
Pablo Nacusse <grandnacusse@gmail.com>
2021-05-27 11:41:00 -0300

    ahora si

---
+++ ejercicio2/ejercicio2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool finalizar = false;
+
+            do
+            {
+                Console.WriteLine("Calculadora");
+                Console.WriteLine("Seleccionar una opcion");
+
+                Console.WriteLine("1.Sumar");
+                Console.WriteLine("2.Restar");
+                Console.WriteLine("3.Multiplicar");
+                Console.WriteLine("4.Dividir");
+                Console.WriteLine("5.Salir");
+
+                int opcion = int.Parse(Console.ReadLine());
+                double resultado = 0;
+
+                Console.WriteLine("Primer numero");
+                double n1 = double.Parse(Console.ReadLine());
+                Console.WriteLine("Segundo numero");
+                double n2 = double.Parse(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case 1:
+                        resultado = n1 + n2;
+                        break;
+                    case 2:
+                        resultado = n1 - n2;
+                        break;
+                    case 3:
+                        resultado = n1 * n2;
+                        break;
+                    case 4:
+                        resultado = n1 / n2;
+                        break;
+                }
+
+                Console.WriteLine("Resultado = {0}", resultado);
+
+                Console.WriteLine("Desea realizar otro calculo?(s/n)");
+                char c = char.Parse(Console.ReadLine());
+
+                if (c == 's')
+                {
+                    finalizar = false;
+                }
+                else
+                {
+                    finalizar = true;
+                }
+
+            } while (!finalizar);
+
+        }
+    }
+}
---
+++ ejercicio3/ejercicio3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ejercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un numero: ");
+            double num = int.Parse(Console.ReadLine());
+
+            double valorAbsoluto = Math.Abs(num);
+            double cuadrado = Math.Pow(num, 2);
+            double raizCuadrada = Math.Sqrt(num);
+            double seno = Math.Sin(num);
+
+        }
+    }
+}
