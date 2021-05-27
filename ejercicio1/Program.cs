1a1887021c346b5f8e6b4e19776b35ee00edec15
Pablo Nacusse <grandnacusse@gmail.com>
2021-05-27 10:38:13 -0300

    ejercicio1

---
+++ ejercicio1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ejercicio1
+{
+    class Program
+    {
+        private static int OrdenInverso(int numero)
+        {
+            int resultado = 0;
+            while (numero > 0)
+            {
+                resultado = resultado * 10 + numero % 10;
+                numero /= 10;
+            }
+            return resultado;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un número: ");
+            int numero = int.Parse(Console.ReadLine());
+            int numeroInverso = OrdenInverso(numero);
+
+            Console.WriteLine("El numero inverso de {0}, es {1}", numero, numeroInverso);
+            Console.ReadLine();
+        }
+    }
+}
