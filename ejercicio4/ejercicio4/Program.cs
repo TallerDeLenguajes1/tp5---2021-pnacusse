33266048a0da225b704c0c8d27a0f3899f647a8d
Pablo Nacusse <grandnacusse@gmail.com>
2021-05-27 12:21:34 -0300

    ejercicio4

---
+++ ejercicio4/ejercicio4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ejercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese una frase");
+            string frase = Console.ReadLine();
+            Console.WriteLine("La frase ingresada es: {0}", frase);
+            Console.WriteLine("Longitud de la cadena = {0}", frase.Length);
+
+        }
+    }
+}
